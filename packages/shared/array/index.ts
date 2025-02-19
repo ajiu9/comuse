36b3142fcb04ebb7fb7b5fa65ef4cc13f2613405
ajiu9 <615944323@qq.com>
2025-02-19 09:52:27 +0800

    feat: add toArray function

--- packages/shared/array/index.ts
+++ packages/shared/array/index.ts
@@ -1,9 +1,10 @@
-import type { Arrayable, Nullable } from 'rollup'
+import type { Arrayable, Nullable } from '../types'
 /**
  * Covert `Arrayable<T>` to `Array<T>`
  *
  * @category Array
  */
 export function toArray<T>(array: Nullable<Arrayable<T>>): Array<T> {
+  array = array ?? []
   return Array.isArray(array) ? array : [array]
 }
