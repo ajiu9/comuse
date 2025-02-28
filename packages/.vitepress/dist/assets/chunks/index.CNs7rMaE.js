48943ee61d9202bdb07b7487600a87fed853d314
ajiu9 <615944323@qq.com>
2025-02-28 16:18:51 +0800

    docs: add docs build files

---
+++ packages/.vitepress/dist/assets/chunks/index.CNs7rMaE.js
@@ -0,0 +1,1 @@
+const s=()=>{};function u(e,t=!1,i="Timeout"){return new Promise((n,o)=>{setTimeout(t?()=>o(i):n,e)})}function r(e){return e()}export{s as N,r as i,u as w};
--- packages/.vitepress/theme/components/FunctionsList.vue
+++ packages/.vitepress/theme/components/FunctionsList.vue
@@ -7,7 +7,7 @@
 <template>
   <div h="1px" bg="$vp-c-divider" m="t-4" />
   <div flex="~" class="children:my-auto" p="2">
-    <i i-carbon-search m="r-2" opacity="50" />
+    <!-- <i i-carbon-search m="r-2" opacity="50" /> -->
     <!-- <input v-model="search" class="w-full" type="text" role="search" placeholder="Search..."> -->
   </div>
 </template>
