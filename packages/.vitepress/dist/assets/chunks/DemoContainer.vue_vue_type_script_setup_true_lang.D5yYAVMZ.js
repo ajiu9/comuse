7df66f7e3a10b0661fb3655ada60a057a066bbfb
ajiu9 <615944323@qq.com>
2025-03-04 16:43:44 +0800

    chore: build:docs

---
+++ packages/.vitepress/dist/assets/chunks/DemoContainer.vue_vue_type_script_setup_true_lang.D5yYAVMZ.js
@@ -0,0 +1,1 @@
+import{d as t,t as a,ag as n,o as r,b as s,f as l,x as c,g as d}from"./framework.CGoBFeW7.js";const _={class:"demo wide"},i={key:0,class:"error"},f=t({__name:"DemoContainer",setup(m){const e=a(null);return n(o=>{e.value=o}),(o,u)=>(r(),s("div",_,[l(o.$slots,"default"),e.value?(r(),s("div",i,c(e.value),1)):d("",!0)]))}});export{f as _};
