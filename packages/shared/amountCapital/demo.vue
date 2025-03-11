40ee6f766c1ae9fe23423b45dc891e4e4fa44fa0
ajiu9 <615944323@qq.com>
2025-03-11 14:00:19 +0800

    feat: add amountCapital demo

--- packages/shared/amountCapital/demo.vue
+++ packages/shared/amountCapital/demo.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import { amountCapital } from 'comuse-shared'
+import { ref } from 'vue'
 
-const num: number = 100.53
+const num = ref(100.53)
+
+const handleInput = (e: Event) => {
+  const input = e.target as HTMLInputElement
+  input.value = input.value.replace(/[^0-9.]/g, '')
+  if (input.value.length > 10)
+    input.value = input.value.slice(0, 10)
+}
 </script>
 
 <template>
-  <note>{{ num }}</note>
+  <input v-model="num" type="number" @input="handleInput">
   <p>amountCapital: {{ amountCapital(num) }}</p>
 </template>
