bfe036a87cf95c93ae10f8689e9833bc7b8c2a4b
ajiu9 <615944323@qq.com>
2025-06-17 17:39:59 +0800

    feat: add uuid method

--- packages/shared/utils/index.ts
+++ packages/shared/utils/index.ts
@@ -175,3 +175,17 @@ export function randomString(length = 8, type: RandomStringType = 'mix'): string
   }
   return result
 }
+
+/**
+ * Browser-side generation of uuid, using v4 method
+ *
+ * @example
+ * ```ts
+ * uuid() // '4222fcfe-5721-4632-bede-6043885be57d'
+ * ```
+ * @returns - uuid
+ */
+export const uuid = (): string =>
+  ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: any) =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16),
+  )
