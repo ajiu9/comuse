dc79a38df6d030886169beac183d83f0a6c2aa14
ajiu9 <615944323@qq.com>
2025-07-25 16:59:59 +0800

    docs: useVConsle

---
+++ packages/integrations/useVConsole/demo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useVConsole } from 'comuse-integrations'
+import { getUrlParam } from 'comuse-shared'
+
+useVConsole()
+</script>
+
+``
+<template>
+  <note>
+    debug: {{ getUrlParam('debug') }}
+  </note>
+</template>
