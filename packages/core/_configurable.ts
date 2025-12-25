f5456e8979ced62d5291f9ce1b73d04e5c11a169
ajiu9 <615944323@qq.com>
2025-12-25 17:09:53 +0800

    docs: build

--- packages/core/_configurable.ts
+++ packages/core/_configurable.ts
@@ -36,6 +36,6 @@ export interface ConfigurableLocation {
 }
 
 export const defaultWindow = isClient ? window : undefined
-export const defaultDocument = isClient ? window?.document : undefined
-export const defaultNavigator = isClient ? window?.navigator : undefined
-export const defaultLocation = isClient ? window?.location : undefined
+export const defaultDocument = isClient ? window.document : undefined
+export const defaultNavigator = isClient ? window.navigator : undefined
+export const defaultLocation = isClient ? window.location : undefined
